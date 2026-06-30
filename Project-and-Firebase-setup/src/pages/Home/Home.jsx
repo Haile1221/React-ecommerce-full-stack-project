630f1226eae21b38741755c6c3197f8f29449fed
Haile1221 <hzeru10@gmail.com>
2026-06-30 11:25:25 +0300

    Full stack ecommerce project with react js

---
+++ Project-and-Firebase-setup/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Home.module.css'
+function Home() {
+  return (
+    <div className={styles.container}>
+  <header className={styles.hero}>
+    <h1>Premium Tech and style</h1>
+    <p>For Beshilo Community</p>
+  </header>
+</div>
+
+  )
+}
+
+export default Home
